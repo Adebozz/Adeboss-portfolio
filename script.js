46c9eaf00b452fada7f45ea048282433a5ed05d9
Adebozz <ademolaadeigbe3@gmail.com>
2023-01-03 19:52:21 +0100

    second commit

--- script.js
+++ script.js
@@ -17,16 +17,12 @@ else{//Hide BacktoTopButton
 }
 
 backToTopButton.addEventListener("click", backToTop);
-
+var menuList = document.getElementsByClassName('navbar')[0];
+menuList.style.maxHeight= "0px"
 
 function clicked(){
     document.getElementsByClassName('navbar')[0].style.display= "flex";
     document.getElementsByClassName('clicked')[0].style.display= "none";
-    document.getElementsByClassName('close')[0].style.display= "flex";
-}
-function close(){
-    document.getElementsByClassName('close')[0].style.color="red";
-    document.getElementsByClassName('navbar')[0].style.display= "none";
-    document.getElementsByClassName('clicked')[0].style.display= "flex";
-    document.getElementsByClassName('close')[0].style.display= "none";
+   
+   
 }
