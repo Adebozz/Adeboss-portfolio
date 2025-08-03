7c93b0a5a6ef420ff961c12b3831b072d2e09343
Adebozz <ademolaadeigbe3@gmail.com>
2025-08-03 19:47:17 +0100

    `about and skills section`

---
+++ my-app/components/About.js
@@ -0,0 +1,36 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div class="homepage" id="about">
+      <div class="about-content">
+        <h1>About me</h1>
+        I am Adeboss, a graduate of computer science. I am an enthusiastic web developer based in Nigeria, with a rich experience in web development & product design. I design beautiful and professional website with years of experience in graphic and web design I have mastered the skills of understanding client requirements according to the latest trends. I have worked with businesses from different niches so you can rely on me for yours <p></p>
+      </div>
+      <div class="about-what">
+        <h2>What i do</h2>
+        From understanding your requirements, to designing a blueprint and delivering the final product, 
+        I do everything that falls in between these lines. What i do are:-
+        <div class="about-contain">
+            <div class="con1">
+                <h3>UI/UX DESIGN</h3>
+                An effective UI/UX is what captures attention and spreads a clear message. I make sure the design is innovative and neat with all of this.
+            </div>
+            <div class="con2">
+                <h3>WEB DEVELOPMENT</h3>
+                If you are looking for a developer who’ll take over the research and development of your website, I am a well-established professional to help you with this.
+
+            </div>
+            <div class="con3">
+                <h3>GRAPHIC DESIGNS</h3>
+                If you are looking for a user-friendly app that will attract more mobile users, I can help you design and build a platform with the latest and trendiest look and feel.
+            </div>
+        </div>
+      </div>
+     
+      </div>
+    </div>
+  )
+}
+
+export default About
